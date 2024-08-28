9cbb4a75916bd9535742d89cbe306fd85b9e5b63
nidhimp131 <nidhimp@unc.edu>
2024-08-28 09:28:14 -0400

    Finished CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""Functions Challenge Question"""
+
+__author__ = "730671459"
+
+
+def mimic(message: str) -> str:
+    """Returns message back to you."""
+    return message
+
+
+def main() -> None:
+    """Prints result of calling mimic function."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
