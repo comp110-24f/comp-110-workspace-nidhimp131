25c0280a4a56fcc04853d3d8d6888bf5b2dd6229
nidhimp131 <nidhimp@unc.edu>
2024-09-07 08:39:20 -0400

    Finished Exercise 01

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,35 @@
+"""Calculates the cost and items needed to throw a tea party."""
+
+__author__ = "730671459"
+
+
+def main_planner(guests: int) -> None:
+    """Prints tea party plan."""
+    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print("Treats: " + str(treats(people=guests)))
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))
+        )
+    )
+
+
+def tea_bags(people: int) -> int:
+    """Determines the number of tea bags needed for a particular number of guests."""
+    return people * 2
+
+
+def treats(people: int) -> int:
+    """Determines the number of treats needed based on number of teas."""
+    return int(tea_bags(people=people) * 1.5)
+
+
+def cost(tea_count: int, treat_count: int) -> float:
+    """Determines total cost of tea bags and treats."""
+    return (tea_count * 0.50) + (treat_count * 0.75)
+
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party?")))
